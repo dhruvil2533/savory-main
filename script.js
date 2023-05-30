e3b57b46914c8bbfcca894ac9c76f3546fb636ea
Dhruvil Modi <77335827+dhruvil2533@users.noreply.github.com>
2023-05-30 16:52:51 +0530

    v4

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+	const minPerSlide = 4
+	let next = el.nextElementSibling
+	for (var i = 1; i < minPerSlide; i++) {
+		if (!next) {
+			// wrap carousel by using first child
+			next = items[0]
+		}
+		let cloneChild = next.cloneNode(true)
+		el.appendChild(cloneChild.children[0])
+		next = next.nextElementSibling
+	}
+})
+
+
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
@@ -333,4 +350,7 @@ const waitForImages = () => {
 };
 
 waitForImages();
+
+/* --------------  review carosal  --------------- */
+
 
